9a1533ab1146839d4873b9505516e1c82d42d4f2
Luiz Vitor Martinez Cardoso <luiz.martinez@geeksys.com.br>
2016-09-26 03:19:27 -0300

    cap

--- cap.c
+++ cap.c
@@ -129,6 +129,7 @@ int v4l2_init_camera(int fd)
 	fmt.fmt.pix.field = V4L2_FIELD_ANY;
 	fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 	fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUV420;
+	//fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_RGB24;
 
 	if (xioctl(fd, VIDIOC_TRY_FMT, &fmt) == -1) {
 		CAP_ERROR_RET("failed trying to set pixel format.");
