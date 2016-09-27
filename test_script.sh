a3bf9fcb80985a54569c01fa1f2bf9dd9abd1f51
Luiz Vitor Martinez Cardoso <luiz.martinez@geeksys.com.br>
2016-09-27 00:02:19 -0300

    test

--- test_script.sh
+++ test_script.sh
@@ -4,13 +4,10 @@ widht=(2592 2048 1920 1600 1280 1280 1024 800 640 320)
 heigth=(1936 1536 1080 1200 960 720 768 600 480 240)
 
 function reload() {
-	sleep .5
-	rmmod vfe_v4l2
-	rmmod ov5640
-	sleep .5
+	modprobe -r -v vfe_v4l2
+	modprobe -r -v ov5640
+	modprobe ov5640 frame_rate="$1"
 	modprobe vfe_v4l2
-	insmod ov5640_build/ov5640.ko frame_rate="$1"
-	sleep .5
 }
 
 function load() {
@@ -25,8 +22,8 @@ function unload() {
 
 for index in ${!widht[*]}
 do
-    reload "$1"
-    echo "TEST $index"
-    echo "Widht : ${widht[$index]}, Heigth : ${heigth[$index]}"
-    sudo ./capture "${widht[$index]}" "${heigth[$index]}"
+	reload_1 "$1"
+	echo "TEST $index"
+	echo "Widht : ${widht[$index]}, Heigth : ${heigth[$index]}"
+	sudo ./cap "${widht[$index]}" "${heigth[$index]}"
 done
